0cdff9703de309e6470896da576490e69acb8d06
Asim Ghaffar <asim.ghaffar@gmail.com>
2022-02-27 08:51:22 +0500

    Update myApp.js
    series completed

--- myApp.js
+++ myApp.js
@@ -1,54 +1,126 @@
 require('dotenv').config();
 
+var mongoose = require('mongoose');
 
-let Person;
+console.log(process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+var Schema = mongoose.Schema;
+
+var PersonSchema = new Schema({
+  name: String,
+  age: Number,
+  favoriteFoods: [String]
+})
+
+// someModel
+let Person = mongoose.model('Person', PersonSchema);
+
+var arrayOfPeople = [
+  {
+    name: 'awesome1',
+    age: 11,
+    favoriteFoods: ['Apple', 'Banana']
+  }, {
+    name: 'awesome2',
+    age: 12,
+    favoriteFoods: ['Orange', 'Banana']
+  }
+];
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  var document = new Person(arrayOfPeople[0]);
+
+  document.save((err, data) => {
+    if (err) return handleError(err);
+    done(null, data);
+  });
+
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) return handleError(err);
+    done(null, data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName }, (err, data) => {
+    if (err) return handleError(err);
+    done(null, data);
+  })
+
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err) return handleError(err);
+    done(null, data);
+  })
+
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) return handleError(err);
+    done(null, data);
+  })
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
+  Person.findById(personId, (err, data) => {
+    if (err) return handleError(err);
+    data.favoriteFoods.push(foodToAdd);
+    data.save((err, moredata) => {
+      if (err) return handleError(err);
+      done(null, moredata);
+    });
+  })
 
-  done(null /*, data*/);
 };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true }, (err, data) => {
+    if (err) return handleError(err);
+    done(null, data);
+  })
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, data) => {
+    if (err) return handleError(err);
+    done(null, data);
+  })
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  let d = Person.remove({ name: nameToRemove }, (err, data) => {
+    if (err) return handleError(err);
+    console.log("data", data);
+    { // workaround because i have a latest version of moogose
+      data.ok = true;
+      data.n = data.deletedCount;
+    }
+    done(null, data);
+  })
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort('name')
+    .limit(2)
+    .select("name favoriteFoods")
+    .exec((err, data) => {
+      if (err) return handleError(err);
+      done(null, data);
+    })
 };
 
 /** **Well Done !!**
